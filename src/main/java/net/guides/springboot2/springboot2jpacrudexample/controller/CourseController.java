bb2c3a7c651a35769b85577efc3a781162a373f8
IfnuAzis <azis.ifnu@gmail.com>
2019-09-26 15:48:21 +0700

    repair course controle

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/CourseController.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/CourseController.java
@@ -18,35 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.guides.springboot2.springboot2jpacrudexample.exception.ResourceNotFoundException;
-import net.guides.springboot2.springboot2jpacrudexample.model.Employee;
-import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepository;
-
-@RestController
-@RequestMapping("/api/v1")
-public class CourseController {
-	@Autowired
-package net.guides.springboot2.springboot2jpacrudexample.controller;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import net.guides.springboot2.springboot2jpacrudexample.exception.ResourceNotFoundException;
-import net.guides.springboot2.springboot2jpacrudexample.model.Employee;
-import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepository;
+import net.guides.springboot2.springboot2jpacrudexample.model.Course;
+import net.guides.springboot2.springboot2jpacrudexample.repository.CourseRepository;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -78,7 +51,7 @@ public class CourseController {
 		Course course = courseRepository.findById(courseId)
 				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id :: " + courseId));
 
-		employee.setnameCourse(courseDetails.getnameCourse());
+		course.setnameCourse(courseDetails.getnameCourse());
 		final Course updatedCourse = courseRepository.save(course);
 		return ResponseEntity.ok(updatedCourse);
 	}
