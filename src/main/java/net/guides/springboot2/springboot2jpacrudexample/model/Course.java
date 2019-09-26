32f8b19d3d96dc8596187ca54499e957b1c71781
Mfaqih <mfaqih918171@gmail.com>
2019-09-26 14:10:36 +0700

    add Course Model

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/Course.java
@@ -0,0 +1,48 @@
+package net.guides.springboot2.springboot2jpacrudexample.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+	private long id;
+	private String nameCourse;
+
+	
+	public Course() {
+		
+	}
+	
+	public Course(String nameCourse, String , String) {
+		this.nameCourse = nameCourse;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "nama_course", nullable = false)
+	public String getnameCourse() {
+		return nameCourse;
+	}
+	public void setnameCourse(String nameCourse) {
+		this.nameCourse = name_Course;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", nameCourse=" + nameCourse + "]";
+	}
+	
+}
